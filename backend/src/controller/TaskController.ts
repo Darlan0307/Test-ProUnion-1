5acd22f7bdcdb8c88df08bcbe1ef96c00cb7d943
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-11 20:30:48 -0300

    feat: implementando o crud completo das tarefas na api

--- backend/src/controller/TaskController.ts
+++ backend/src/controller/TaskController.ts
@@ -1,17 +1,55 @@
-// Tipagem das tarefas
-type Task = {
-  id: number;
-  title: string;
-  status: string;
-};
+import { Request, Response } from "express";
+import { Status, Task } from "../@types/type-task";
 
 // Array de objetos da tarefa
 const tasks: Task[] = [];
 
 //
 export class TaskController {
-  // Adiciona uma nova tarefa
-  // Retorna todas as tarefas
-  // Atualiza uma tarefa
+  // criando uma nova tarefa
+  async addTask(req: Request, res: Response) {
+    try {
+      const { title } = req.body;
+      // Adicionando uma tarefa na lista
+      tasks.push({ id: tasks.length + 1, title, status: Status.pendente });
+      res.status(201).json({ message: "Tarefa adicionada com sucesso" });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao adicionar tarefa" });
+    }
+  }
+  // Retornando todas as tarefas
+  async getAllTasks(req: Request, res: Response) {
+    try {
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao recuperar tarefas" });
+    }
+  }
+
+  // Atualizando uma tarefa
+  async updateTask(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { title, status } = req.body;
+      // Atualizando a tarefa na lista
+      const index = Number(id) - 1;
+      tasks[index].title = title;
+      tasks[index].status = status;
+      res.status(200).json({ message: "Tarefa atualizada com sucesso" });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao atualizar tarefa" });
+    }
+  }
   // Deleta uma tarefa
+  async deleteTask(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      // Deletando a tarefa na lista
+      const index = Number(id) - 1;
+      tasks.splice(index, 1);
+      res.status(200).json({ message: "Tarefa deletada com sucesso" });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao deletar tarefa" });
+    }
+  }
 }
