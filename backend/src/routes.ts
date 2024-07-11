cb894ec27b1d2bc8e665fe1d26255e43e3ca1ef5
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-11 20:31:47 -0300

    feat: criando todas as rotas para usar o controller das tarefas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,25 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { TaskController } from "./controller/TaskController";
 // Criando objeto do router para poder registrar rotas
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+// Criando uma instancia da classe TaskController
+const taskController = new TaskController();
+
+// Rota padrão
+router.get("/", (_, res) => {
+  res.send("API de tarefas");
 });
+// Rota para listar todas as tarefas
+router.get("/tasks", taskController.getAllTasks);
+
+// Rota para criar uma nova tarefa
+router.post("/tasks", taskController.addTask);
+
+// Rota para atualizar uma tarefa
+router.put("/tasks/:id", taskController.updateTask);
+
+// Rota para deletar uma tarefa
+router.delete("/tasks/:id", taskController.deleteTask);
 
 export default router;
